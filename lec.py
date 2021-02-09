cac46ca164ced8089c74cf7cee6897463dbc148f
MikSm1th <thiscuriousquest@gmail.com>
2021-02-08 19:43:25 -0500

    first commit

---
+++ lec.py
@@ -0,0 +1,15 @@
+'''Declarative Knowledge is composed of a statement of fact.
+
+-- A good health care plan improves the quality of medical care while saving money. 
+
+-- y is the square root of x if and only if y*y=x
+
+-- Imperative Knowledge 
+
+
+'''
+a = 10 
+
+print(a)
+
+
