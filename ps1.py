be6bd36864d80401342daa8e9801a53ca59796a9
MikSm1th <thiscuriousquest@gmail.com>
2021-02-23 21:50:15 -0500

    gitbsh commit

--- ps1.py
+++ ps1.py
@@ -11,9 +11,6 @@ interest_paid = (annual_interest/12) * balance
 principle_paid = min_monthly_pay - interest_paid
 remaining_balance = balance - principle_paid
 
-
-
-
 if __name__ == "__main__":
     print("Program output:\n") 
     print("minimun_monthly_payment = ",  min_monthly_pay)        
